49377fb5f4cba4b2555a12657fdb3d10c1c74ef3
Md Jubayer Hossain <71443320+jubayerhossain625@users.noreply.github.com>
2023-12-29 14:12:40 +0600

    fix to place order button height done all by me

--- lib/app/data/commonData.dart
+++
@@ -1,8 +0,0 @@
-class Data{
-  static const String watch1='https://i.ibb.co/PjXbkrh/watch.png';
-  static const String watch2='https://i.ibb.co/bNm0KDY/watch2.png';
-  static const String cashOn='https://shorturl.at/bnBHJ';
-  static const String cashOn1='https://i.ibb.co/92WH3gn/cashon.png';
-  static const String user='https://i.ibb.co/JsZqFX9/photo-1535713875002-d1d0cf377fde.jpg';
-
-}
